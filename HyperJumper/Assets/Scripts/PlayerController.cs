e3f313924fb9377fab668e2e7c6ed59e13208a8a
Vex0on <Iplayagame101@gmail.com>
2023-11-20 17:05:13 +0100

    Script for turning around if space is pressed once and jumping if space is hold

--- HyperJumper/Assets/Scripts/PlayerController.cs
+++ HyperJumper/Assets/Scripts/PlayerController.cs
@@ -1,27 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("GameObjects")]
     public GameObject arrow;
+    public GameObject player;
+
+    [Header("Stats")]
+    public float maxJumpHigh = 2f;
+    public float maxJumpForce = 5f;
+    public float maxForwardForce = 3.5f;
+    public float jumpForceMultiplier = 0.8f;
+    public float shortPressTime = 0.2f;
+
+    private bool isSpaceHold = false;
+    private bool hasPerformedRotation = false;
+    private float spaceHoldTime = 0f;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        KeyCode spaceKey = KeyCode.Space;
+
+        if (Input.GetKeyDown(spaceKey))
         {
-            Debug.Log("Click");
-            PlayerChangeDirection();
+            isSpaceHold = true;
+            hasPerformedRotation = false;
+            spaceHoldTime = 0f;
+        }
+
+        if (Input.GetKey(spaceKey) && isSpaceHold)
+        {
+            spaceHoldTime += Time.deltaTime;
+
+            if (spaceHoldTime >= shortPressTime && !hasPerformedRotation)
+            {
+                Jump();
+                isSpaceHold = false;
+                spaceHoldTime = 0f;
+            }
+        }
+
+        if (Input.GetKeyUp(spaceKey) && isSpaceHold)
+        {
+            DirectionRotation();
+            hasPerformedRotation = true;
+            isSpaceHold = false;
+            spaceHoldTime = 0f;
         }
     }
 
-    void PlayerChangeDirection()
+    void DirectionRotation()
     {
-        transform.Rotate(Vector3.up, 180f);
+        player.transform.Rotate(Vector3.up, 180f);
 
         if (arrow != null)
         {
-            arrow.transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+            arrow.transform.rotation = Quaternion.Euler(0, player.transform.eulerAngles.y, 0);
         }
+    }
+
+    void Jump()
+    {
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+
+        Vector2 direction = new Vector2(player.transform.right.x, player.transform.right.y);
+        Vector2 jumpVector = new Vector2(direction.x, direction.y + 0.5f);
+
+        rb.velocity = jumpVector * CalculateJumpForce() + direction * maxForwardForce;
+
+        spaceHoldTime = 0f;
+    }
+
+    float CalculateJumpForce()
+    {
+        float jumpForceHeight = Mathf.Clamp(spaceHoldTime, 0f, maxJumpHigh);
+        return (maxJumpForce + jumpForceHeight * jumpForceMultiplier);
     }
 }
