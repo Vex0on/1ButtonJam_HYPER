07f1ed297964381476603bcf87508ba950e8bd73
Filip Gorczyca <filipekbwo@wp.pl>
2023-11-21 15:06:54 +0100

    moved to new input system with changed jumping

--- HyperJumper/Assets/Scripts/PlayerController.cs
+++ HyperJumper/Assets/Scripts/PlayerController.cs
@@ -1,63 +1,47 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     [Header("GameObjects")]
     [SerializeField] private GameObject arrow;
     [SerializeField] private GameObject player;
 
     [Header("Stats")]
-    [SerializeField] private float maxJumpHigh = 2f;
-    [SerializeField] private float maxJumpForce = 5f;
-    [SerializeField] private float jumpForceMultiplier = 0.5f;
-    [SerializeField] private float shortPressTime = 0.2f;
+    [SerializeField] private float _maxJumpHigh = 2f;
+    [SerializeField] private float _maxJumpForce = 5f;
+    [SerializeField] private float _jumpForceMultiplier = 0.5f;
+    [SerializeField] private float _shortPressTime = 0.2f;
 
-    private bool isSpaceHold = false;
-    private bool hasPerformedRotation = false;
-    private float spaceHoldTime = 0f;
-    private Rigidbody2D rb;
+    [SerializeField] private double _spaceHoldTime = 0f;
 
     void Start()
     {
         rb = player.GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    public void Jump(InputAction.CallbackContext context)
     {
-        KeyCode spaceKey = KeyCode.Space;
-
-        if (Input.GetKeyDown(spaceKey))
+        switch (context.phase)
         {
-            isSpaceHold = true;
-            hasPerformedRotation = false;
-            spaceHoldTime = 0f;
-        }
+            case InputActionPhase.Started:
+                _spaceHoldTime = context.time;
+                break;
+            case InputActionPhase.Canceled:
+                var time = context.time - _spaceHoldTime;
+                Debug.Log(time);
+                if (_spaceHoldTime < _shortPressTime)
+                    DirectionRotation();
+                else
+                    Jump();
 
-        if (Input.GetKey(spaceKey) && isSpaceHold)
-        {
-            spaceHoldTime += Time.deltaTime;
-        }
-
-        if (Input.GetKeyUp(spaceKey) && isSpaceHold)
-        {
-            if (spaceHoldTime < shortPressTime)
-            {
-                DirectionRotation();
-                hasPerformedRotation = true;
-            }
-            else if (spaceHoldTime >= shortPressTime && !hasPerformedRotation)
-            {
-                Jump();
-            }
-
-            isSpaceHold = false;
-            spaceHoldTime = 0f;
+                break;
         }
     }
 
-
-
-    void DirectionRotation()
+    private void DirectionRotation()
     {
         player.transform.Rotate(Vector3.up, 180f);
 
@@ -67,8 +51,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-
-    void Jump()
+    private void Jump()
     {
         float jumpForce = CalculateJumpForce();
 
@@ -80,14 +63,13 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(jumpForceVector, ForceMode2D.Impulse);
 
-        spaceHoldTime = 0f;
+        _spaceHoldTime = 0f;
     }
 
-
-    float CalculateJumpForce()
+    private float CalculateJumpForce()
     {
-        float jumpForceHeight = Mathf.Clamp(spaceHoldTime, 0f, maxJumpHigh);
-        float jumpForce = (maxJumpForce + jumpForceHeight * jumpForceMultiplier);
+        float jumpForceHeight = Mathf.Clamp((float)_spaceHoldTime, 0f, _maxJumpHigh);
+        float jumpForce = (_maxJumpForce + jumpForceHeight * _jumpForceMultiplier);
 
         return jumpForce;
     }
