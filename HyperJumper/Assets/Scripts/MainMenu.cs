2ef062c34581706858d60171ebf668c947d7412f
Vex0on <Iplayagame101@gmail.com>
2023-11-23 02:39:21 +0100

    Setup basic options panel template

--- HyperJumper/Assets/Scripts/MainMenu.cs
+++ HyperJumper/Assets/Scripts/MainMenu.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject buttons;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private Toggle fullscreenToggle;
+    [SerializeField] private TMP_Dropdown resolutionPicker;
+    [SerializeField] private TMP_InputField playerNickname;
     public bool isGameStarted = false;
 
 
@@ -27,11 +33,29 @@ public class MainMenu : MonoBehaviour
     public void Options()
     {
         Debug.Log("Options");
+        optionsPanel.SetActive(true);
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void ToggleFullscreen()
+    {
+        Screen.fullScreen = fullscreenToggle.isOn;
+    }
+
+    public void ChangeResolution()
+    {
+        Resolution resolution = Screen.resolutions[resolutionPicker.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetPlayerName()
+    {
+        string playerName = playerNickname.text;
+        Debug.Log("Player Name: " + playerName);
     }
 }
