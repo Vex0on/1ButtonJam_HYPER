9740d27d820116f6c8851ff78fb6a90ef6e9b31e
Kalimedess <kalimedesss@gmail.com>
2023-11-24 23:55:52 +0100

    Latestchanges

--- HyperJumper/Assets/Scripts/PlayerController.cs
+++ HyperJumper/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private SpriteRenderer playerSprite;
     private Rigidbody2D rb;
 
     [Header("GameObjects")]
@@ -20,9 +21,30 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private double _startSpaceHoldTime;
     [SerializeField] private double _spaceHoldTime;
 
+    [Header("Block-related variables")]
+    [SerializeField] private bool _isTouchingStickyBlock;
+    [SerializeField] private bool _isTouchingFireBlock;
+    [SerializeField] private float _desiredTimeToChangeSpriteColor = 5f;
+    [SerializeField] private float _stickyBlockSlowdown = 100f;
+    [SerializeField] private float _stickyBlockJumpIncrease = 0.5f;
+    [SerializeField] private float _stickyBlockVerticalIncrease = 0.5f;
+    [SerializeField] private float _fireBlockJumpIncrease = 1.5f;
+    [SerializeField] private float _fireBlockVerticalIncrease = 1.5f;
+    [Header("Tracker vars")]
+    [SerializeField] private float _currentBlockJumpIncrease = 1f;
+    [SerializeField] private float _currentBlockVerticalIncrease = 1f;
+    [SerializeField] private float _savedDrag = 0;
+    [SerializeField] private float _timeElapsedStandingOnBlock;
+    [Header("VFX")]
+    [SerializeField] private ParticleSystem _fireParticles;
+    [SerializeField] private ParticleSystem _honeyParticles;
+
+
     void Start()
     {
         rb = player.GetComponent<Rigidbody2D>();
+        playerSprite = player.GetComponent<SpriteRenderer>();
+
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -44,7 +66,12 @@ public class PlayerController : MonoBehaviour
                 if (_spaceHoldTime < _shortPressTime)
                     DirectionRotation();
                 else
+                {
+                    _isTouchingStickyBlock = false;
+                    _isTouchingFireBlock = false;
                     Jump();
+                    rb.drag = _savedDrag;
+                }
 
                 break;
         }
@@ -68,7 +95,7 @@ public class PlayerController : MonoBehaviour
         float horizontalMultiplier = 0.5f;
 
         Vector2 jumpDirection = player.transform.right;
-        Vector2 jumpForceVector = new(jumpDirection.x * jumpForce * horizontalMultiplier, jumpForce * verticalMultiplier);
+        Vector2 jumpForceVector = new Vector2(jumpDirection.x * jumpForce * horizontalMultiplier * _currentBlockVerticalIncrease, jumpForce * verticalMultiplier * _currentBlockJumpIncrease);
 
         rb.AddForce(jumpForceVector, ForceMode2D.Impulse);
     }
@@ -80,5 +107,93 @@ public class PlayerController : MonoBehaviour
 
         return jumpForce;
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out StickyBlockFunctionality s))
+        {
+            _isTouchingStickyBlock = true;
+            _savedDrag = rb.drag;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out StickyBlockFunctionality s))
+        {
+            _isTouchingStickyBlock = true;
+        }
+        if (collision.TryGetComponent(out FireBlockFunctionality f))
+        {
+            _isTouchingFireBlock = true;
+        }
+    }
+    private void Update()
+    {
+        if(_isTouchingFireBlock && !_isTouchingStickyBlock)
+        {
+            _timeElapsedStandingOnBlock += Time.deltaTime;
+            IncreaseFireBuff();
+            _fireParticles.Emit(1);
+        }
+        else
+        {
+            DecreaseFireBuff();
+        }
+        if (_isTouchingStickyBlock && !_isTouchingFireBlock)
+        {
+            _timeElapsedStandingOnBlock += Time.deltaTime;
+            IncreaseStickyDebuff();
+            _honeyParticles.Emit(1);
+        }
+        else
+        {
+            DecreaseStickyDebuff();
+        }
+        if(!_isTouchingStickyBlock && !_isTouchingFireBlock)
+        {
+            _timeElapsedStandingOnBlock -= Time.deltaTime;
+        }
+    }
+    private void IncreaseFireBuff()
+    {
+        playerSprite.color = Color.Lerp(Color.white, Color.red, _timeElapsedStandingOnBlock / _desiredTimeToChangeSpriteColor);
 
+        if (_currentBlockJumpIncrease < _fireBlockJumpIncrease)
+            _currentBlockJumpIncrease += 0.005f;
+
+        if (_currentBlockVerticalIncrease < _fireBlockVerticalIncrease)
+            _currentBlockVerticalIncrease += 0.005f;
+    }
+    private void DecreaseFireBuff()
+    {
+        playerSprite.color = Color.Lerp(Color.red, Color.white, _timeElapsedStandingOnBlock / _desiredTimeToChangeSpriteColor);
+
+        if (_currentBlockJumpIncrease > 1f)
+            _currentBlockJumpIncrease -= 0.005f;
+
+        if (_currentBlockVerticalIncrease > 1f)
+            _currentBlockVerticalIncrease -= 0.005f;
+    }
+    private void IncreaseStickyDebuff()
+    {
+        playerSprite.color = Color.Lerp(Color.white, Color.yellow, _timeElapsedStandingOnBlock / _desiredTimeToChangeSpriteColor);
+
+        if (rb.drag < _stickyBlockSlowdown)
+            rb.drag += 3;
+
+        if (_currentBlockJumpIncrease >= _stickyBlockJumpIncrease)
+            _currentBlockJumpIncrease -= 0.005f;
+
+        if (_currentBlockVerticalIncrease >= _stickyBlockVerticalIncrease)
+            _currentBlockVerticalIncrease -= 0.005f;
+    }
+    private void DecreaseStickyDebuff()
+    {
+        playerSprite.color = Color.Lerp(Color.yellow, Color.white, _timeElapsedStandingOnBlock / _desiredTimeToChangeSpriteColor);
+
+        if (_currentBlockJumpIncrease < 1f)
+            _currentBlockJumpIncrease += 0.005f;
+
+        if (_currentBlockVerticalIncrease < 1f)
+            _currentBlockVerticalIncrease += 0.005f;
+    }
 }
